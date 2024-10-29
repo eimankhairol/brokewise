816519a4db199eeb6ae88cf1c6f0011340244275
Khairol Eiman <eiman@Khairols-MacBook-Air.local>
2024-10-29 13:12:15 -0400

    Updated database for MYSQL

--- brokewise_django/settings.py
+++ brokewise_django/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'brokewise_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': brokewise / 'db.sqlite3',
+        'USER': 'eiman',
+        'PASSWORD': '2101',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
